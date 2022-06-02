7d454f0dd595ca58e7886555d08ab31265729674
rontalledo12 <rontalledo@gmail.com>
2022-06-02 14:52:29 +0800

    room number updates

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -32,7 +32,7 @@ function Chat({ socket, name, room }) {
     return (
         <div className='chat-window'>
             <div className='chat-header'>
-              <p><center>Welcome to Chat Room </center></p>
+              <p><center>Welcome to Room: {room}</center></p>
             </div>
             <div className='chat-body'>
                 <ScrollToBottom className='message-container'>
